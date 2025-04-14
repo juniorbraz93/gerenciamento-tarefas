10376004e02def14b7e73eabf1b287bf8f462edd
juniorbraz93 <jbraz.est@gmail.com>
2025-04-14 12:55:30 -0300

    ajusted

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://user:password@localhost/todo_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:Zildar1!@localhost:3306/db_gerenciador_tarefas")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = ("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
